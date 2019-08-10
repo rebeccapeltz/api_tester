d67673f35888c20c91144b2d0fe6e5426f4cc659
rebeccapeltz <rebeccapeltz@gmail.com>
2019-08-10 09:22:08 -0700

    first commit

---
+++ src/views/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="test">
+     {{results}}
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Test",
+  data () {
+    return {
+      results: null,
+      errors: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("//numbersapi.com/42")
+      .then(response => {
+        this.results = response.data;
+      })
+      .catch(error => {
+        this.errors.push(error);
+      });
+  }
+
+
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
